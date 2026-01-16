83f856651877a4cf2815f6e8dac94ed1808215cc
Cauã <cauasampaiovolei@gmail.com>
2026-01-16 10:50:16 -0300

    build: finalizando projeto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -3,7 +3,7 @@ namespace Hotelaria.Models
     public class Reserva()
     {
         private List<Pessoa> _hospedes = new List<Pessoa>();
-        private Suite _suite;
+        private Suite? _suite;
         private int _diasReservados;
 
         public int DiasReservados 
@@ -21,14 +21,24 @@ namespace Hotelaria.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            _hospedes.AddRange(hospedes);
-            Console.WriteLine("Hospedes adicionados!");
+            if( _suite != null && _suite.Capacidade >= hospedes.Count)
+            {
+                _hospedes.AddRange(hospedes);                
+                Console.WriteLine("\nHospedes adicionados!");
+            }else if(_suite != null)
+            {                
+                Console.WriteLine($"\nA suite tem capacidade para apenas {_suite.Capacidade} hospedes");
+            }
+            else
+            {
+                Console.WriteLine($"\nVocê precisa cadastrar uma suite primeiro!");
+            }
         }
 
         public void CadstrarSuite(Suite suite)
         {
             _suite = suite;
-            Console.WriteLine("Suite cadastrada com sucesso!");
+            Console.WriteLine("\nSuite cadastrada com sucesso!");
         }
 
         public int ObterQuantidadeDeHosepdes()
@@ -38,8 +48,26 @@ namespace Hotelaria.Models
 
         public decimal CalcularValorDiaria()
         {
-
-            return _suite.ValorDiaria * _diasReservados;
+            if(_suite != null && _hospedes.Count > 0)
+            {
+                decimal valorTotal = _suite.ValorDiaria * _diasReservados;
+                if(_diasReservados >= 10)
+                {
+                    return valorTotal - (valorTotal * 0.10M);
+                }
+                else
+                {
+                    return _suite.ValorDiaria * _diasReservados;                                    
+                }
+            }
+            else if(_hospedes.Count < 0)
+            {
+                throw new ArgumentException("\nVocê precisa ter hospedes para calcular a diária");
+            }
+            else
+            {
+                throw new ArgumentException("\nVocê precisa ter uma suite cadastrada!");
+            }
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,40 +1,96 @@
 ﻿using Hotelaria.Models;
 
 bool sistemaRodando = true;
-string menu = "1 - Cadastrar Hospedes\n2 - Cadastrar Suite\n3 - Ver quantiade de hospedes\n4 - Encerrar diária\n5 - Sair";
+string menu = "\n1 - Cadastrar Suite\n2 - Cadastrar Hospedes\n3 - Ver quantiade de hospedes\n4 - Encerrar diária\n5 - Sair";
 
-Reserva reserva = new Reserva();
-
+Console.Clear();
+Reserva  reserva = new Reserva();
 while(sistemaRodando)
 {
 
     Console.WriteLine(menu);
     ConsoleKeyInfo opcao = Console.ReadKey();
-
-    switch(opcao.KeyChar)
+    try
     {
-        case '1':
+        switch(opcao.KeyChar)
+        {
+            case '1':
 
-            Console.WriteLine("Digite o nome dos hospedes separados por espaço");
+                Console.WriteLine("\nDigite o tipo de suite que deseja cadastrar");
+                string tipoSuite = Console.ReadLine();
+                if(tipoSuite == "")
+                {
+                    throw new ArgumentException("O tipo de suite deve ser informado corretamente!");
+                }
+                Console.WriteLine("\nDigite a capacidade da Suite");
+                int.TryParse(Console.ReadLine(), out int capacidade);
+                if(capacidade <= 0)
+                {
+                    throw new ArgumentException("A suite deve ter capacidade maior que 0");
+                }
+                Console.WriteLine("\nDigite o valor da diária: ");
+                decimal.TryParse(Console.ReadLine(), out decimal valorDiaria);
 
-            string stringHospedes = Console.ReadLine();
-            List<Pessoa> hospedes = new List<Pessoa>();
+                if(valorDiaria <= 0)
+                {
+                    throw new ArgumentException("O valor da diária deve ser maior que 0");
+                }
+                Suite suite = new Suite(tipoSuite, capacidade, valorDiaria);
 
-            List<string> listaHospedes = new List<string>(stringHospedes.Split(" "));
+                reserva.CadstrarSuite(suite);
 
-            break;  
-        case '2':
-            break;
-        case '3':
-            break;
-        case '4':            
-            break;
-        case '5':
-            Console.WriteLine("Saindo do sistema!");
-            Environment.Exit(0);
-            break;
-        default:
-            Console.Clear();            
-            break;
+                break;
+            case '2':
+                            
+                Console.WriteLine("\nQuantos dias irão ficar hospedados?");
+                int.TryParse(Console.ReadLine(), out int diasReservados);
+
+                reserva.DiasReservados = diasReservados;  
+
+                Console.WriteLine("\nDigite o nome dos hospedes separados por virgula (,)");
+
+                string stringHospedes = Console.ReadLine();
+                List<Pessoa> hospedes = new List<Pessoa>();
+
+                List<string> listaHospedes = new List<string>(stringHospedes.Split(","));
+
+                foreach(string hospede in listaHospedes)
+                {
+                    if(!hospede.Contains(' '))
+                    {
+                        throw new ArgumentException("\nDeve informar o sobrenome do hospede!");                        
+                    }
+                    string[] dadosHospede = hospede.Split(" ");
+                    Pessoa pessoa = new Pessoa(dadosHospede[0], dadosHospede[1]);
+                    hospedes.Add(pessoa);
+                }
+
+                reserva.CadastrarHospedes(hospedes);            
+
+                break;  
+
+            case '3':
+                Console.WriteLine($"\nNesta reserva temos {reserva.ObterQuantidadeDeHosepdes()} pessoas hospedadas");
+                break;
+            case '4': 
+                Console.WriteLine($"\nO valor total da diária ficou em {reserva.CalcularValorDiaria():C2}");           
+                break;
+            case '5':
+                Console.WriteLine("\nSaindo do sistema!");
+                sistemaRodando = false;
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("\nOpção inválida...");
+                break;
+        }
+        
+    }catch(Exception exception)
+    {
+        Console.WriteLine(exception.Message);
     }
+
+    Console.WriteLine("\nPressione qualquer tecla para continuar");
+    Console.ReadKey();
+    Console.Clear();
 }
